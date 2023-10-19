76b2a4163f2bee9097c0531c2d9551712ea1411c
Christwelve <meng.christian28@gmail.com>
2023-10-19 17:56:48 +0200

    without alloc

--- src/exec/exec_chain.c
+++ src/exec/exec_chain.c
@@ -21,57 +21,55 @@ int	exec_chain(t_chain *chain, int *fd_stdin, int *fd_stdout)
 	int				ports[2];
 	} t_exchain;
 
-	t_exchain *ex = (t_exchain *)malloc(sizeof(t_exchain));
-    if (ex == NULL) 
-		error_fatal();
+	t_exchain ex;
 
-	ex->i = 0;
-	ex->fd = 0;
-	ex->pid = 0;
+	ex.i = 0;
+	ex.fd = 0;
+	ex.pid = 0;
 
 	// TODO: protect this crap, also rename vars cause they seem to be replaced by compiler (see debug vars)
 	// i, chain, stdin, stdout, exit_code, pid
-	while (ex->i < arr_size(&chain->commands))
+	while (ex.i < arr_size(&chain->commands))
 	{
 		// chain, cmd, i
-		ex->raw = (t_raw_command *) arr_get(&chain->commands, ex->i);
-		if (converter_convert(ex->raw, &ex->cmd))
+		ex.raw = (t_raw_command *) arr_get(&chain->commands, ex.i);
+		if (converter_convert(ex.raw, &ex.cmd))
 		{
 			if (arr_size(&chain->commands) == 1)
-				return (free(ex), global()->exit_code);
-			SKIP(ex->i);
+				return (global()->exit_code);
+			SKIP(ex.i);
 		}
 		// chain, cmd, i, ports, exit_code, stdin, stdout
 		// // cmd, i, chain, ports
-		if (ex->cmd.type == COMMAND_NONE)
-			SKIP(ex->i);
-		if ((ex->i < arr_size(&chain->commands) - 1) && (fd_pipe(ex->ports)))
+		if (ex.cmd.type == COMMAND_NONE)
+			SKIP(ex.i);
+		if ((ex.i < arr_size(&chain->commands) - 1) && (fd_pipe(ex.ports)))
 			error_fatal();
 		// // chain, cmd, exit_code, stdin, stdout
-		if (arr_size(&chain->commands) == 1 && ex->cmd.type != COMMAND_EXTERNAL)
+		if (arr_size(&chain->commands) == 1 && ex.cmd.type != COMMAND_EXTERNAL)
 		{
-			ex->exit_code = run_builtin_in_main(&ex->cmd);
+			ex.exit_code = run_builtin_in_main(&ex.cmd);
 			dup_std_end(fd_stdin, fd_stdout);
-			return (free(ex), ex->exit_code);
+			return (ex.exit_code);
 		}
 		//pid, cmd, ports, i, chain
 		else
 		{
-			ex->pid = fork();
-			if (ex->pid == -1)
+			ex.pid = fork();
+			if (ex.pid == -1)
 				error_fatal();
-			if (ex->pid == 0)
-				run_child(&ex->cmd, ex->fd, ex->ports, ex->i == arr_size(&chain->commands) - 1);
+			if (ex.pid == 0)
+				run_child(&ex.cmd, ex.fd, ex.ports, ex.i == arr_size(&chain->commands) - 1);
 			else
-				run_parent(&ex->cmd, &ex->fd, ex->ports);
+				run_parent(&ex.cmd, &ex.fd, ex.ports);
 		}
-		ex->i++;
+		ex.i++;
 	}
-	waitpid(ex->pid, &ex->exit_code, 0);
-	ex->i--;
-	while (ex->i--)
+	waitpid(ex.pid, &ex.exit_code, 0);
+	ex.i--;
+	while (ex.i--)
 		wait(NULL);	
-	return (free(ex), ex->exit_code >> 8);
+	return (ex.exit_code >> 8);
 }
 
 
