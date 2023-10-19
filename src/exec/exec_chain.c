ae1dc7d260df9d89d9f0cf0eef935c9d5a117271
Christwelve <meng.christian28@gmail.com>
2023-10-19 17:51:50 +0200

    exec_chain solution with struct

--- src/exec/exec_chain.c
+++ src/exec/exec_chain.c
@@ -1,5 +1,6 @@
 #include <unistd.h>
 #include <sys/wait.h>
+#include <stdlib.h>
 #include "skipdef.h"
 #include "minishell.h"
 #include "array.h"
@@ -10,59 +11,125 @@
 
 int	exec_chain(t_chain *chain, int *fd_stdin, int *fd_stdout)
 {
+	typedef struct s_ex_chain {
 	int				exit_code;
 	pid_t			pid;
 	unsigned long	i;
 	int				fd;
+	t_raw_command	*raw;
+	t_command		cmd;
+	int				ports[2];
+	} t_exchain;
 
+	t_exchain *ex = (t_exchain *)malloc(sizeof(t_exchain));
+    if (ex == NULL) 
+		error_fatal();
 
-	fd = 0;
-	i = 0;
-	pid = 0;
+	ex->i = 0;
+	ex->fd = 0;
+	ex->pid = 0;
+
 	// TODO: protect this crap, also rename vars cause they seem to be replaced by compiler (see debug vars)
 	// i, chain, stdin, stdout, exit_code, pid
-	while (i < arr_size(&chain->commands))
+	while (ex->i < arr_size(&chain->commands))
 	{
-		t_raw_command	*raw;
-		t_command		cmd;
-		int				ports[2];
 		// chain, cmd, i
-		raw = (t_raw_command *) arr_get(&chain->commands, i);
-		if (converter_convert(raw, &cmd))
+		ex->raw = (t_raw_command *) arr_get(&chain->commands, ex->i);
+		if (converter_convert(ex->raw, &ex->cmd))
 		{
 			if (arr_size(&chain->commands) == 1)
-				return (global()->exit_code);
-			SKIP(i);
+				return (free(ex), global()->exit_code);
+			SKIP(ex->i);
 		}
 		// chain, cmd, i, ports, exit_code, stdin, stdout
 		// // cmd, i, chain, ports
-		if (cmd.type == COMMAND_NONE)
-			SKIP(i);
-		if ((i < arr_size(&chain->commands) - 1) && (fd_pipe(ports)))
+		if (ex->cmd.type == COMMAND_NONE)
+			SKIP(ex->i);
+		if ((ex->i < arr_size(&chain->commands) - 1) && (fd_pipe(ex->ports)))
 			error_fatal();
 		// // chain, cmd, exit_code, stdin, stdout
-		if (arr_size(&chain->commands) == 1 && cmd.type != COMMAND_EXTERNAL)
+		if (arr_size(&chain->commands) == 1 && ex->cmd.type != COMMAND_EXTERNAL)
 		{
-			exit_code = run_builtin_in_main(&cmd);
+			ex->exit_code = run_builtin_in_main(&ex->cmd);
 			dup_std_end(fd_stdin, fd_stdout);
-			return (exit_code);
+			return (free(ex), ex->exit_code);
 		}
 		//pid, cmd, ports, i, chain
 		else
 		{
-			pid = fork();
-			if (pid == -1)
+			ex->pid = fork();
+			if (ex->pid == -1)
 				error_fatal();
-			if (pid == 0)
-				run_child(&cmd, fd, ports, i == arr_size(&chain->commands) - 1);
+			if (ex->pid == 0)
+				run_child(&ex->cmd, ex->fd, ex->ports, ex->i == arr_size(&chain->commands) - 1);
 			else
-				run_parent(&cmd, &fd, ports);
+				run_parent(&ex->cmd, &ex->fd, ex->ports);
 		}
-		i++;
+		ex->i++;
 	}
-	waitpid(pid, &exit_code, 0);
-	i--;
-	while (i--)
-		wait(NULL);
-	return (exit_code >> 8);
+	waitpid(ex->pid, &ex->exit_code, 0);
+	ex->i--;
+	while (ex->i--)
+		wait(NULL);	
+	return (free(ex), ex->exit_code >> 8);
 }
+
+
+// int	exec_chain(t_chain *chain, int *fd_stdin, int *fd_stdout)
+// {
+// 	int				exit_code;
+// 	pid_t			pid;
+// 	unsigned long	i;
+// 	int				fd;
+
+
+// 	fd = 0;
+// 	i = 0;
+// 	pid = 0;
+// 	// TODO: protect this crap, also rename vars cause they seem to be replaced by compiler (see debug vars)
+// 	// i, chain, stdin, stdout, exit_code, pid
+// 	while (i < arr_size(&chain->commands))
+// 	{
+// 		t_raw_command	*raw;
+// 		t_command		cmd;
+// 		int				ports[2];
+// 		// chain, cmd, i
+// 		raw = (t_raw_command *) arr_get(&chain->commands, i);
+// 		if (converter_convert(raw, &cmd))
+// 		{
+// 			if (arr_size(&chain->commands) == 1)
+// 				return (global()->exit_code);
+// 			SKIP(i);
+// 		}
+// 		// chain, cmd, i, ports, exit_code, stdin, stdout
+// 		// // cmd, i, chain, ports
+// 		if (cmd.type == COMMAND_NONE)
+// 			SKIP(i);
+// 		if ((i < arr_size(&chain->commands) - 1) && (fd_pipe(ports)))
+// 			error_fatal();
+// 		// // chain, cmd, exit_code, stdin, stdout
+// 		if (arr_size(&chain->commands) == 1 && cmd.type != COMMAND_EXTERNAL)
+// 		{
+// 			exit_code = run_builtin_in_main(&cmd);
+// 			dup_std_end(fd_stdin, fd_stdout);
+// 			return (exit_code);
+// 		}
+// 		//pid, cmd, ports, i, chain
+// 		else
+// 		{
+// 			pid = fork();
+// 			if (pid == -1)
+// 				error_fatal();
+// 			if (pid == 0)
+// 				run_child(&cmd, fd, ports, i == arr_size(&chain->commands) - 1);
+// 			else
+// 				run_parent(&cmd, &fd, ports);
+// 		}
+// 		i++;
+// 	}
+// 	waitpid(pid, &exit_code, 0);
+// 	i--;
+// 	while (i--)
+// 		wait(NULL);
+// 	return (exit_code >> 8);
+// }
