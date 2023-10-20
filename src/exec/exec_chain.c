38643f020a128b6fa80a7b23b211e7bdbc228475
Christwelve <meng.christian28@gmail.com>
2023-10-20 10:37:23 +0200

    exec_chain fully restructured

--- src/exec/exec_chain.c
+++ src/exec/exec_chain.c
@@ -9,9 +9,8 @@
 #include "global.h"
 #include "fd.h"
 
-int	exec_chain(t_chain *chain, int *fd_stdin, int *fd_stdout)
+typedef struct s_ex_chain
 {
-	typedef struct s_ex_chain {
 	int				exit_code;
 	pid_t			pid;
 	unsigned long	i;
@@ -19,56 +18,78 @@ int	exec_chain(t_chain *chain, int *fd_stdin, int *fd_stdout)
 	t_raw_command	*raw;
 	t_command		cmd;
 	int				ports[2];
-	} t_exchain;
+}	t_exchain;
 
-	t_exchain ex;
+static void	fork_procs(
+		t_chain *chain, t_exchain *ex, int *fd_stdin, int *fd_stdout)
+{
+	ex->pid = fork();
+	if (ex->pid == -1)
+		error_fatal();
+	if (ex->pid == 0)
+		run_child(&ex->cmd, ex->fd, ex->ports,
+			ex->i == arr_size(&chain->commands) - 1);
+	else
+		run_parent(&ex->cmd, &ex->fd, ex->ports);
+}
+
+static int	handle_single_builtin(
+		t_chain *chain, t_exchain *ex, int *fd_stdin, int *fd_stdout)
+{
+	if (arr_size(&chain->commands) == 1 && ex->cmd.type != COMMAND_EXTERNAL)
+	{
+		ex->exit_code = run_builtin_in_main(&ex->cmd);
+		dup_std_end(fd_stdin, fd_stdout);
+		return (1);
+	}
+	return (0);
+}
+
+int	exec_loop(t_chain *chain, t_exchain *ex, int *fd_stdin, int *fd_stdout)
+{
+	while (ex->i < arr_size(&chain->commands))
+	{
+		ex->raw = (t_raw_command *) arr_get(&chain->commands, ex->i);
+		if (converter_convert(ex->raw, &ex->cmd))
+		{
+			if (arr_size(&chain->commands) == 1)
+				return (1);
+			ex->i++;
+			continue ;
+		}
+		if (ex->cmd.type == COMMAND_NONE)
+		{
+			ex->i++;
+			continue ;
+		}
+		if ((ex->i < arr_size(&chain->commands) - 1) && (fd_pipe(ex->ports)))
+			error_fatal();
+		if (handle_single_builtin(chain, ex, fd_stdin, fd_stdout))
+			return (2);
+		else
+			fork_procs(chain, ex, fd_stdin, fd_stdout);
+		ex->i++;
+	}
+}
+
+int	exec_chain(t_chain *chain, int *fd_stdin, int *fd_stdout)
+{
+	t_exchain	ex;
+	int			ret;
 
 	ex.i = 0;
 	ex.fd = 0;
 	ex.pid = 0;
-
-	// TODO: protect this crap, also rename vars cause they seem to be replaced by compiler (see debug vars)
-	// i, chain, stdin, stdout, exit_code, pid
-	while (ex.i < arr_size(&chain->commands))
-	{
-		// chain, cmd, i
-		ex.raw = (t_raw_command *) arr_get(&chain->commands, ex.i);
-		if (converter_convert(ex.raw, &ex.cmd))
-		{
-			if (arr_size(&chain->commands) == 1)
-				return (global()->exit_code);
-			SKIP(ex.i);
-		}
-		// chain, cmd, i, ports, exit_code, stdin, stdout
-		// // cmd, i, chain, ports
-		if (ex.cmd.type == COMMAND_NONE)
-			SKIP(ex.i);
-		if ((ex.i < arr_size(&chain->commands) - 1) && (fd_pipe(ex.ports)))
-			error_fatal();
-		// // chain, cmd, exit_code, stdin, stdout
-		if (arr_size(&chain->commands) == 1 && ex.cmd.type != COMMAND_EXTERNAL)
-		{
-			ex.exit_code = run_builtin_in_main(&ex.cmd);
-			dup_std_end(fd_stdin, fd_stdout);
-			return (ex.exit_code);
-		}
-		//pid, cmd, ports, i, chain
-		else
-		{
-			ex.pid = fork();
-			if (ex.pid == -1)
-				error_fatal();
-			if (ex.pid == 0)
-				run_child(&ex.cmd, ex.fd, ex.ports, ex.i == arr_size(&chain->commands) - 1);
-			else
-				run_parent(&ex.cmd, &ex.fd, ex.ports);
-		}
-		ex.i++;
-	}
+	ret = 0;
+	ret = exec_loop(chain, &ex, fd_stdin, fd_stdout);
+	if (ret == 1)
+		return (global()->exit_code);
+	else if (ret == 2)
+		return (ex.exit_code);
 	waitpid(ex.pid, &ex.exit_code, 0);
 	ex.i--;
 	while (ex.i--)
-		wait(NULL);	
+		wait(NULL);
 	return (ex.exit_code >> 8);
 }
 
