f01b9578bab9e1295f6899321ca33731718141c7
Christwelve <meng.christian28@gmail.com>
2023-10-22 14:43:53 +0200

    fixed Ctrl+D print exit on line

--- src/signals/signals.c
+++ src/signals/signals.c
@@ -2,16 +2,8 @@
 #include <unistd.h>
 #include <readline/readline.h>
 #include <signal.h>
+#include "error.h"
 #include "minishell.h"
-
-static void	ctrld(int sig)
-{
-	if (sig == SIGQUIT)
-	{
-		rl_replace_line("exit", 0);
-		rl_redisplay();
-	}
-}
 
 static void	ctrlc(int sig)
 {
@@ -27,5 +19,5 @@ static void	ctrlc(int sig)
 void	signals(void)
 {
 	signal(SIGINT, &ctrlc);
-	signal(SIGQUIT, &ctrld);
+	signal(SIGQUIT, SIG_IGN);
 }
